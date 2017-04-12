69975717bed65cf2d5140b44c02cb28f4774cae0
borellig <geoffrey.borelli@gmail.com>
2017-04-12 15:36:06 +0200

    Premier test Serveur 12/04/2017-15:35

--- APERO/src/pages/home/home.ts
+++ APERO/src/pages/home/home.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  posts:any;
+
   constructor(public navCtrl: NavController) {
+
+
+
+    var xhr = new XMLHttpRequest();
+    	// envoi requete
+    	xhr.open("GET", "http://leofazendeiro.fr/HelloWorld.php", true);
+    	xhr.send(null);
+
+    	xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4 && xhr.status == 200){
+				/*var rep = xhr.responseText;
+				alert(rep);*/
+				var rep = JSON.parse(xhr.responseText);
+        console.log("rep : "+rep.nomClient+rep.listePizzas);
+			}
+      else {
+        console.log("else");
+      }
+		}
 
   }
 
